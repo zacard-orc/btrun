b6fc18b3feed41ab85a94d28f93a080dafa7afe1
13816025562@139.com <13816025562@139.com>
2019-03-06 12:06:55 +0800

    0.1.20:2019年K线

---
+++ btc_runpolicy5.py
@@ -0,0 +1,126 @@
+# -*- coding:utf-8 -*-
+'''
+根据5分钟的MA30，MA60，略线上下位置来比对策
+'''
+
+from __future__ import division
+from __future__ import unicode_literals
+
+import os, json, sys, time, datetime, traceback
+import numpy as np
+from libs import mHTTP, mBusiLog, mUtil, mEnv, mDBA2, mEmail
+from threading import Timer
+
+insdb = mDBA2.A_SDB()
+logger = mBusiLog.myLog(os.path.basename(__file__).split('.')[0] + '.log')
+last_vol_delta = 0
+
+# base para
+dom_url = 'https://api.huobi.pro'
+AccessKeyId = '08f4bb11-da3cb5d9-81292c66-f7372'
+SignatureMethod = 'HmacSHA256'
+SignatureVersion = '2'
+
+# policy参数
+# gf_pol_name='pol_ma30'
+# gf_tlz=0.999 #贪婪值，修正MA附近的凸起
+# gf_ma20defense=1.002
+
+gf_pol = [{'pol_name': 'pol_buy_ma30_offset', 'pol_desc': 'ma30下方买入', 'pol_threshold1': -0.3},
+          {'pol_name': 'pol_buy_rushup', 'pol_desc': '快速上涨开口向上', 'pol_threshold1': 0.5},
+          {'pol_name': 'pol_sell_ma30_ffset', 'pol_desc': '跌穿MA30', 'pol_threshold1': 0.3},
+          {'pol_name': 'pol_sell_inflex', 'pol_desc': 'MA30上方出现拐点', 'pol_threshold1': 0.1}, ]
+
+# kp_coin=['btcusdt','ethusdt','ltcusdt','eosusdt','neousdt','etcusdt']
+# kp_coin=['btcusdt','ethusdt','bchusdt','htusdt','xrpusdt','eosusdt']
+kp_coin = ['ethusdt']
+
+
+def api_getRunData(mode='real'):
+    try:
+        if mode != 'real':
+            insdb.sBtcClearDevOps()
+
+        for i in range(len(kp_coin)):
+            o = {}
+            kp = kp_coin[i]
+            o['env'] = os.getenv('PYVV')
+            o['kp'] = kp
+            o['exn'] = 'HB'
+            o['vol'] = 1
+            if mode == 'real':
+                rtn = insdb.sLoadKLineForPolicyLast(o)
+            else:
+                rtn = insdb.sLoadKLineForPolicyHis(o)
+
+            for j in range(len(rtn)):
+                real_rtn = rtn[j]
+                # msg = kp + ',ma30 downarea,'+str(real_rtn['close'])
+                # logger.debug(msg)
+                # mEmail.sendEmail(msg, msg)
+                '''
+                场景一：
+                价格在maXX下方0.3%
+                '''
+                logger.debug(str(j))
+                cond1 = (real_rtn['close'] - real_rtn['p_ma30']) * 100 / real_rtn['p_ma30']
+                cond2 = real_rtn['angle_v_ma5'] > 5
+                if cond1 <= -1 and cond2:
+                    msg = kp + ',ma30 downarea,'+str(real_rtn['close'])
+                    logger.debug(msg)
+                    mEmail.sendEmail(msg, msg)
+
+                '''
+                场景二：
+                应对单边突然上涨，对于上面场景的补充，最新价格在MA30上方0.5附件，三线一致开口
+                '''
+                cond1 = real_rtn['angle_v_ma5'] > 0 and real_rtn['angle_v_ma30'] > 0
+                cond2 = real_rtn['angle_v_ma5'] > real_rtn['angle_v_ma30']
+                cond3 = real_rtn['angle_v_ma5'] >= 45
+
+                if cond1 and cond2 and cond3:
+                    msg = kp + ',rush up,'+str(real_rtn['close'])
+                    logger.debug(msg)
+                    mEmail.sendEmail(msg, msg)
+
+                '''
+                场景三：
+                超过MA30多少以上就直接卖
+                '''
+                # cond1 = real_rtn['open'] > real_rtn['p_ma30']
+                # cond2 = real_rtn['close'] < real_rtn['p_ma30']
+                # if cond1 and cond2:
+
+
+
+                '''
+                场景四：
+                M30上方出现拐点
+                '''
+                cond1 = real_rtn['angle_v_ma5'] < -20
+                # cond2=real_rtn['angle_v_ma5']<real_rtn['angle_v_ma30']
+                # cond3 = (real_rtn['close'] - real_rtn['p_ma30']) * 100 / real_rtn['p_ma30']
+                cond4 = real_rtn['close'] > real_rtn['p_ma30']
+                if cond1 and cond4:
+                    msg = kp + ',ma30 up inflex point,'+str(real_rtn['close'])
+                    logger.debug(msg)
+                    mEmail.sendEmail(msg, msg)
+
+
+
+    except Exception, e:
+        print traceback.format_exc()
+        logger.debug('[HS],' + traceback.format_exc())
+
+
+
+if os.getenv('PYVV') == 'dev':
+    api_getRunData(mode='real')
+
+
+if os.getenv('PYVV') == 'work':
+    while True:
+        api_getRunData(mode='real')
+
+
+
---
+++ libs/mEmail.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+import urllib, urllib2,logging
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+username = '13816025562'
+sender = '13816025562@139.com'
+password = 'love1985'
+smtp_server = 'smtp.139.com'
+
+receivers = ','.join(['llysonylin2012@163.com'])
+
+
+def sendEmail(in_sub, in_content):
+    # 邮件内容
+    msg = MIMEMultipart()
+    msg['Subject'] = in_sub.encode('utf-8')
+    msg['From'] = sender
+    msg['To'] = receivers
+
+    # 文字部分
+    puretext = MIMEText('')
+    msg.attach(puretext)
+
+    try:
+        client = smtplib.SMTP()
+        client.connect(smtp_server)
+        client.login(username, password)
+        client.sendmail(sender, receivers, msg.as_string())
+        client.quit()
+        logging.debug('邮件已发送')
+    except Exception,e:
+        logging.debug(e.message)
+
+# sendEmail(u'你好','')
